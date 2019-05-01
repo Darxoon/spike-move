05ee727a74aee22cb27a5b09f3bef4a8ce3acb23
Darxoon <Jakob.priemer@gmail.com>
2019-05-01 15:22:03 +0200

    changed how the tooltip follows the selection

--- Assets/_Scripts/TooltipFollow.cs
+++ Assets/_Scripts/TooltipFollow.cs
@@ -11,13 +11,12 @@ public class TooltipFollow : MonoBehaviour
     [Header("World Coords to Canvas Coords")]
 
     [SerializeField] private Vector3 offset;
-    [SerializeField] private Canvas canvas;
-    [SerializeField] private float scale;
+    [SerializeField] private Camera mainCam;
 
 
     private void Update()
     {
         // go to selection
-        transform.localPosition = Vector3.Lerp(transform.position, grid.CellToWorld(selection.gridCoords) + offset, transition) * canvas.scaleFactor * scale;
+        transform.localPosition = Vector3.Lerp(transform.position, mainCam.ViewportToScreenPoint(mainCam.WorldToViewportPoint(grid.CellToWorld(selection.gridCoords)) + offset), transition);
     }
 }
